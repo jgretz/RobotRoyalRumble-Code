729e9e273c5f3736083078ab1b1864836724abff
jgretz <jgretz@truefitsolutions.com>
2016-07-13 10:24:47 -0400

    add firmata.net example

---
+++ c#/1/robo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace Robo
+{
+  class Program
+  {
+    const int speed = 50;
+    const int sleep = 1000;
+
+    static void Main(string[] args)
+    {
+      using (var robot = new Bot())
+      {
+        robot.Connect("COM3", 9600);
+
+        string input = string.Empty;
+        while (input == string.Empty)
+        {
+          robot.SetSpeeds(speed, speed);
+          Console.WriteLine("Move Forward");
+
+          Thread.Sleep(sleep);
+
+          robot.SetSpeeds(-1 * speed, -1 * speed);
+          Console.WriteLine("Move Back");
+
+          Thread.Sleep(sleep);
+
+          robot.SetSpeeds(0, 0);
+
+          input = Console.ReadLine();
+        }
+      }
+    }
+  }
+}
